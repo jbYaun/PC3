d3b4926bc5ebad5b10622d6c896863a78d4515f5
Jean  Yaun <briice.101114@gmail.com>
2021-06-21 18:11:59 -0500

    practica calificada 3

---
+++ src/main/java/com/examen/pc3/Pc3Application.java
@@ -0,0 +1,13 @@
+package com.examen.pc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pc3Application.class, args);
+    }
+
+}
